cd54602263bb041b5fcaaab37cc48a84efb43d4c
ilhams26 <ilhamsimarmata.26@gmail.com>
2026-06-11 10:52:51 +0700

    Run DAST

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,8 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'check.role' => \App\Http\Middleware\CheckRole::class,
             
         ]);
-        // Fix DAST: Content Security Policy (CSP) Header Not Set [Medium - CWE-693]
-        // Menambahkan header Content-Security-Policy ke semua HTTP response
+        // Fix DAST
         $middleware->append(\App\Http\Middleware\ContentSecurityPolicy::class);
     })  
     
