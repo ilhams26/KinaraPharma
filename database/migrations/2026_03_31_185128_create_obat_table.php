3355c9f0697b5f2b267c53778dc2afb17aa00e8c
ilhams26 <ilhamsimarmata.26@gmail.com>
2026-09-14 00:59:40 +0700

    ubah struktur tabel

--- database/migrations/2026_03_31_185128_create_obat_table.php
+++ database/migrations/2026_03_31_185128_create_obat_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('stok_minimum')->default(10);
             $table->string('foto')->nullable();
             $table->timestamps();
-            $table->text('efek_samping')->nullable();
         });
     }
 
